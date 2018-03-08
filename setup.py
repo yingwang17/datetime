bc3ec39b1e7bf794b71dfe37d34a18dceb4f77dc
yingwang17 <36865721+yingwang17@users.noreply.github.com>
2018-03-08 08:08:09 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup( name='datetime',
+       version='0.1',
+       description='This module expands how to use datetime functions',
+       url='',                # usually a github URL
+       author='Ying Wang',
+       license='Ying Wang',
+       packages=['datetime'],
+       install_requires=[ ],
+       zip_safe=False )
